b687e5cc4a1a395bbd4c20a088d6b5b72091cf74
snmaddula <snmaddula@outlook.com>
2018-12-21 05:07:47 +0530

    Initial Commit

--- src/main/java/snmaddula/pochub/simplemsapp/SimpleMsApp.java
+++ src/main/java/snmaddula/pochub/simplemsapp/SimpleMsApp.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleMsAppApplication {
+public class SimpleMsApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleMsAppApplication.class, args);
+		SpringApplication.run(SimpleMsApp.class, args);
 	}
 
 }
-
---
+++ src/main/java/snmaddula/pochub/simplemsapp/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package snmaddula.pochub.simplemsapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello from Simple MS App";
+	}
+}
