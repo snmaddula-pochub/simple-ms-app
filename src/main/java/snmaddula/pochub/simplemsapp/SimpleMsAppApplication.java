99f70c346820e45f39f36a3204447e75bcd6bc7f
snmaddula <snmaddula@outlook.com>
2018-12-21 05:02:23 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/pochub/simplemsapp/SimpleMsAppApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.pochub.simplemsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMsAppApplication.class, args);
+	}
+
+}
+
