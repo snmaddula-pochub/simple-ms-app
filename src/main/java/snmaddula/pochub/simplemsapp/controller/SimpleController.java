096f5457b66978f090e34ae151b7dd5599363c1d
snmaddula <snmaddula@outlook.com>
2018-12-22 10:34:39 +0530

    Added headers support

--- src/main/java/snmaddula/pochub/simplemsapp/controller/SimpleController.java
+++ src/main/java/snmaddula/pochub/simplemsapp/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package snmaddula.pochub.simplemsapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class SimpleController {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello from Simple MS App";
+		return "Hello from Simple MS App!";
+	}
+	
+	@GetMapping("/hello2")
+	public String helloWithHeader(@RequestHeader("x-msg") String msg) {
+		return "Hello from Simple MS App! [X-MSG=" + msg + "]";
 	}
 }
