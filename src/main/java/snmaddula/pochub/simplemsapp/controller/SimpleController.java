7432ea6388b192e13c3c2b3feebb6ae3e0e70fe5
snmaddula <snmaddula@outlook.com>
2018-12-22 06:19:21 +0530

    v1 done

--- src/main/java/snmaddula/pochub/simplemsapp/controller/SimpleController.java
+++ src/main/java/snmaddula/pochub/simplemsapp/controller/SimpleController.java
@@ -1,13 +1,22 @@
 package snmaddula.pochub.simplemsapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+	
+	@Value("${server.port}")
+	private String port;
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello from Simple MS App";
+		return "Hello from Simple MS App running on port : " + port;
+	}
+	
+	@GetMapping("/")
+	public String status() {
+		return "Up";
 	}
 }
